cc864f71ce3c2d1e381ffbbefe9bd19ad8d34379
opaz <opaz@mp3-bdx.local>
2015-02-15 22:42:06 +0100

    Many small resources

--- app/js/resource.js
+++ app/js/resource.js
@@ -1,5 +1,5 @@
 function Resource() {
-  var RESOURCE_RADIUS = 50;
+  var RESOURCE_RADIUS = 3;
   var RESOURCE_COLOR = 0xFF0000;
   
   var agent = new Agent("Resource", RESOURCE_RADIUS, RESOURCE_COLOR);
